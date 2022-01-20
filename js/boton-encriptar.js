51a2b5162158653d38c787e70aa55b4986746985
juanpablo051982 <98069508+juanpablo051982@users.noreply.github.com>
2022-01-20 16:44:37 -0300

    Update boton-encriptar.js
    cambio de mayúscula a minúscula y sacar los acentos.

--- js/boton-encriptar.js
+++ js/boton-encriptar.js
@@ -2,11 +2,27 @@ var encriptar= document.querySelector(".boton-encriptar");
 encriptar.addEventListener("click",function(){
 
 var input = document.getElementById("texto-encriptar").value;
+input =input.toLowerCase();
 var mensaje = Array.from(input);
     
 for (var i= 0;i <mensaje.length;i++){
     var letras = mensaje[i];
-    
+     
+    if (letras =="á"){
+        mensaje[i]="ai"
+    }
+    if (letras =="é"){
+        mensaje[i]="enter"
+    }
+    if (letras == "í"){
+        mensaje[i]="imes"
+    }
+    if (letras =="ó"){
+        mensaje[i]="ober"
+    }
+    if (letras =="ú"){
+        mensaje[i]="ufat"
+    }
     if (letras =="a" ){
         mensaje[i] = "ai";
     }
